77182b3c720edd4cd5585f43cea41da32887707f
J. Ellis <jgellis63@gmail.com>
2022-10-14 17:43:00 -0600

    lots of games

--- nba-service.js
+++ nba-service.js
@@ -1,20 +1,42 @@
 let url = "https://www.balldontlie.io/api/v1/games";
-fetch(url)
-    .then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      console.log(json);
-      let results = "";
-       for (let i=0; i < json.data.length; i++)
-       {
-           results += "<div> <p>Home: " + json.data[i].home_team.full_name + "  Away: "+ json.data[i].visitor_team.full_name +"</p>";
-           results += "<p> Score" + json.data[i].home_team_score + "  Away: "+ json.data[i].visitor_team_score + "</p></div>";
-       }
-       document.getElementById("Match").innerHTML = results;
-       
-      });
-      
-      
+// fetch(url)
+//     .then(function (response) {
+//         return response.json();
+//     }).then(function (json) {
+//         console.log(json);
+//         let results = "";
+//         for (let i = 0; i < json.data.length; i++) {
+//             results += "<div> <p>Home: " + json.data[i].home_team.full_name + "  Away: " + json.data[i].visitor_team.full_name + "</p>";
+//             results += "<p> Score" + json.data[i].home_team_score + "  Away: " + json.data[i].visitor_team_score + "</p></div>";
+//         }
+//         document.getElementById("Match").innerHTML = results;
+
+//     });
 
 
+async function getAllGames() {
+    let pages = [...Array(20).keys()];
+    let allData = [];
 
+    await Promise.all(
+        pages.map(async p => {
+            var url = new URL("https://www.balldontlie.io/api/v1/games");
+            url.searchParams.append("page", p);
+            url.searchParams.append("per_page", 100);
+            const res = await fetch(url).then(data => { return data.json(); });
+            allData.push(res);
+        })
+    );
+    console.log(allData);
+    let results = "";
+    for (let j = 0; j < allData.length; j++) {
+        let json = allData[j];
+        for (let i = 0; i < json.data.length; i++) {
+            results += "<div> <p>Home: " + json.data[i].home_team.full_name + "  Away: " + json.data[i].visitor_team.full_name + "</p>";
+            results += "<p> Score" + json.data[i].home_team_score + "  Away: " + json.data[i].visitor_team_score + "</p></div>";
+        }
+    }
+    document.getElementById("Match").innerHTML = results;
+
+}
+getAllGames();
